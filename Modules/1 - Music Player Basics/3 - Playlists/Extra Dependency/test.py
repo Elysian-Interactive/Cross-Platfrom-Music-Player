c59c491a1d5c41edbccf0d83909c29197fefd851
Elysian-Interactive <officialelysianinteractive@gmail.com>
2024-03-06 22:02:34 +0530

    Finishing Playlist

---
+++ Modules/1 - Music Player Basics/3 - Playlists/Extra Dependency/test.py
@@ -0,0 +1,10 @@
+from List import List
+
+new_list = List()
+new_list.append("A",10)
+new_list.append("B",20)
+new_list.append("C",30)
+
+new_list.display()
+print(new_list.getKeys())
+new_list.display()
